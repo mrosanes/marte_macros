1753e4cecd79f4e12c16b528937b73e364587e7e
Marc Rosanes <marc.rosanes@gtd.eu>
2020-04-11 18:48:45 +0000

    Simulate the inputs of the macro
    Simulate the inputs that would come from the MacroExecutor
    to the MARTe library, in order to test the library.

---
+++ macros_lib/martecfglib_test.py
@@ -0,0 +1,117 @@
+from martecfglib import MarteConfig
+
+if __name__ == '__main__':
+    
+    apps = [
+    [
+        'AppTest',  # App Name
+        'RealTimeApplication',  # Class
+        [  # functions (One note but yet it is required to be a repeat node)
+            [
+                "ReferenceContainer",
+                [
+                    [
+                        "GAMTimer",
+                        "IOGAM1",
+                        [
+                            [
+                                "Time",
+                                "Timer",
+                                "uint32",
+                            ],
+                            [
+                                "Count",
+                                "Counter",
+                                "uint32",
+                            ],
+                        ],
+                        [
+                            [
+                                "outtime",
+                                "outTimer",
+                                "float",
+                            ],
+                            [
+                                "outcount",
+                                "outCounter",
+                                "float",
+                            ],
+                        ],                        
+                    ],
+                ],
+
+            ],
+        ],
+                            
+        [
+            [
+                "ReferenceContainer",
+                "DDB1",
+                "GAMDataSource",
+                "LoggerDataSource",
+                "TimingDataSource",
+                [
+                    [
+                        "Timer",
+                        "LinuxTimer",
+                        "Default",
+                        [
+                            [
+                                "Counter",
+                                "uint32",
+                            ],
+                            [
+                                "Time",
+                                "uint32",
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        
+
+        [  # States (One note but yet it is required to be a repeat node)
+            [
+                "ReferenceContainer",
+                [
+                    [
+                        "State1",    
+                        "RealTimeState",
+                        [
+                            [
+                                "ReferenceContainer",
+                                [
+                                    [
+                                        "Thread1",
+                                        "RealTimeThread",
+                                        "0x1",
+                                        [
+                                            ["GAMTimer"],
+                                            ["GAMTimer2"],
+                                            ["GAMTimer3"],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+
+            ],
+        ],
+               
+        # scheduler
+        [
+            [
+                "GAMScheduler",
+                "Timings",
+            ],
+        ],
+    ]
+    ]
+            
+    FILE_NAME = './martecfg.txt'
+    martecfg_obj = MarteConfig(FILE_NAME, apps)
+    martecfg_obj.generate_file()
+
